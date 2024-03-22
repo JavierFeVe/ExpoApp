02dcbc946ad3e0fec1ded0e6a0be69b76e9be4ee
JavierFeVe <127032921+JavierFeVe@users.noreply.github.com>
2024-03-22 17:55:34 +0100

    search and filter

--- App.js
+++ App.js
@@ -1,25 +1,12 @@
-import { Button,View,Text,FlatList} from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator} from "@react-navigation/drawer";
-import { createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {Platform} from "react-native";
-import Home from "./pages/Home";
-import News from "./pages/News";
-import Settings from "./pages/Settings";
+import React from "react";
+import { View } from "react-native";
 import styles from "./styles";
-
-const data = new Array(100)
-  .fill(null)
-  .map((v,i) => ({key: i.toString(),value: `Item ${i}`
-  }));
+import ListContainer from "./ListContainer";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FlatList
-      data={data}
-      renderItem={({item}) => <Text style={styles.item}>{item.value}</Text>}
-      />
+      <ListContainer />
     </View>
   );
 }
--- styles.js
+++ styles.js
@@ -6,11 +6,29 @@ export default StyleSheet.create({
     flexDirection: "column",
     paddingTop: 40,
   },
-  item:{
-    margin:5,
-    padding:5,
-    color:"slategrey",
+
+  item: {
+    margin: 5,
+    padding: 5,
+    color: "slategrey",
+    backgroundColor: "ghostwhite",
+    textAlign: "center",
+  },
+
+  filter: {
+    height: 40,
+    width: 200,
+  },
+
+  controls: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  ListSort: {
     backgroundColor:"ghostwhite",
-    textAlign:"center",
   }
 });
